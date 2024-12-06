411bb23dd9ab1e9e70022f7be3672511be266e1a
ntdatt812 <ntdat812@gmail.com>
2024-12-06 20:55:28 +0700

    39

--- src/layout.tsx
+++ src/layout.tsx
@@ -3,9 +3,10 @@ import AppHeader from "components/layout/app.header"
 import { useEffect } from "react"
 import { fetchAccountAPI } from "services/api"
 import { useCurrentApp } from "components/context/app.context"
+import SyncLoader from "react-spinners/SyncLoader"
 
 function Layout() {
-  const { setUser, isAppLoading, setIsAppLoading } = useCurrentApp();
+  const { setUser, isAppLoading, setIsAppLoading, setIsAuthenticated } = useCurrentApp();
 
   useEffect(() => {
     const fetchAccount = async () => {
@@ -13,15 +14,28 @@ function Layout() {
       console.log("Check account: ", res)
       if (res.data) {
         setUser(res.data.user);
+        setIsAuthenticated(true)
       }
       setIsAppLoading(false);
     }
     fetchAccount()
   }, [])
   return (
-    <>
-      <AppHeader />
-      <Outlet />
+    <>{isAppLoading == false ?
+      <div>
+        <AppHeader />
+        <Outlet />
+      </div>
+      :
+      <div style={{
+        position: "fixed", top: "50%", left: "50%",
+        transform: "translate(-50%, -50%)"
+      }}>
+        <SyncLoader
+          size={30}
+          color="#7FFFD4" />
+      </div >
+    }
     </>
   )
 }
