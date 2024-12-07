7dc49992cf246ea73b9f81ec474972293b451a9b
ntdatt812 <ntdat812@gmail.com>
2024-12-07 10:49:36 +0700

    44

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -15,7 +15,7 @@ declare global {
             pages: number;
             total: number;
         },
-        results: T[]
+        result: T[]
     }
 
     interface ILogin {
@@ -49,6 +49,17 @@ declare global {
 
     interface IFetchAccount {
         user: IUser
+    }
 
+    interface IUserTable {
+        _id: string,
+        fullName: string,
+        email: string,
+        phone: string,
+        role: string,
+        avatar: string,
+        isActive: boolean,
+        createdAt: Date,
+        updatedAt: Date,
     }
 }
